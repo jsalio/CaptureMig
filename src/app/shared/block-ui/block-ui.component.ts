10fdb48ebc67aa2529c75020aae5fe7e64a0a9d3
Jorge Rodriguez <jrodriguez@novosit.com>
2025-06-03 15:18:11 -0400

    fix lkogin and block ui message

--- src/app/shared/block-ui/block-ui.component.ts
+++ src/app/shared/block-ui/block-ui.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-block-ui',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './block-ui.component.html',
   styleUrls: ['./block-ui.component.css']
 })
